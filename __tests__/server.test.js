3cd9bdb6285c9a57bb93304dbb830a5de2a31e87
Luis Lopez <metfanforever@gmail.com>
2026-08-17 00:53:07 -0700

    mod server.test add gitignore

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -13,4 +13,30 @@ describe('Express Server', () => {
     expect(response.status).toEqual(404);
   });
 
+  test('returns a 404 on a bad method', async () => {
+    const response = await request.post('/person?name=fred');
+
+    expect(response.status).toEqual(404);
+  });
+
+  test('returns a 500 if no name is in the query string', async () => {
+    const response = await request.get('/person');
+  
+    expect(response.status).toEqual(500);
+  });
+
+  test('returns a 200 if the name is in the query string', async () => {
+    const response = await request.get('/person?name=fred');
+  
+    expect(response.status).toEqual(200);
+  });
+  
+  test('returns the correct name object', async () => {
+    const response = await request.get('/person?name=fred');
+  
+    expect(response.body).toEqual({
+      name: 'fred',
+    });
+  });
+
 });
